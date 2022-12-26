b5aaac6cd4395ed91ed73b76e3b84779d0ee4ec2
Kennet <kennet@seebruecke.org>
2022-12-26 01:37:08 +0100

    Create Dockerfile

--- next.config.js
+++ next.config.js
@@ -2,6 +2,9 @@
 const nextConfig = {
   reactStrictMode: true,
   swcMinify: true,
+
+  // Needed to build the project as a standalone app inside the Docker image.
+  output: 'standalone',
 };
 
 module.exports = nextConfig;
