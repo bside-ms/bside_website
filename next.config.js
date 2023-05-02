d661ad8c83ca74ebf468ef37ccf03dfd264034c3
Kennet <kennet@seebruecke.org>
2023-05-02 19:54:29 +0200

    Add image domain

--- next.config.js
+++ next.config.js
@@ -3,6 +3,10 @@ const nextConfig = {
   reactStrictMode: true,
   swcMinify: true,
 
+  images: {
+    domains: ['localhost', process.env.PAYLOAD_URL],
+  },
+
   // Needed to build the project as a standalone app inside the Docker image.
   output: 'standalone',
 };
