4fa4f1cbe648491f979a22bd7c3d91b1d8cc606d
Kennet <kennet@seebruecke.org>
2023-05-09 18:05:23 +0200

    Home: Display menu on top

--- components/common/HeaderBar.tsx
+++ components/common/HeaderBar.tsx
@@ -23,30 +23,52 @@ const HeaderBar = ({ isVisible = true }: Props): ReactElement => {
     );
 
     return (
-        <div
-            className="fixed top-0 left-0 right-0 z-10 bg-white transition-opacity duration-200"
-            style={isVisible ? { opacity: '100%' } : { opacity: 0, pointerEvents: 'none' }}
-        >
-            <div className="lg:w-[60rem] lg:mx-auto">
-                <div>
-                    <div className="p-4 flex justify-between">
-                        <div
-                            className="w-6 md:w-8 md:cursor-pointer md:hover:text-orange-500"
-                            onClick={handleClickOnHeart}
-                        >
-                            <Heart />
-                        </div>
+        <>
 
-                        <div
-                            className="w-6 md:w-8 md:cursor-pointer md:hover:text-orange-500"
-                            onClick={toggleNavigation}
-                        >
-                            <BurgerHeart />
+            <div
+                className="fixed top-0 left-0 right-0 z-10 bg-transparent transition-opacity duration-200"
+                style={isVisible ? { opacity: '0' } : { opacity: 1 }}
+            >
+                <div className="lg:w-[60rem] lg:mx-auto">
+                    <div>
+                        <div className="p-4 flex justify-end">
+                            <div
+                                className="w-6 md:w-8 md:cursor-pointer md:hover:text-orange-500"
+                                onClick={toggleNavigation}
+                            >
+                                <BurgerHeart />
+                            </div>
                         </div>
                     </div>
                 </div>
             </div>
-        </div>
+
+            <div
+                className="fixed top-0 left-0 right-0 z-10 bg-white transition-opacity duration-200"
+                style={isVisible ? { opacity: '100%' } : { opacity: 0, pointerEvents: 'none' }}
+            >
+                <div className="lg:w-[60rem] lg:mx-auto">
+                    <div>
+                        <div className="p-4 flex justify-between">
+                            <div
+                                className="w-6 md:w-8 md:cursor-pointer md:hover:text-orange-500"
+                                onClick={handleClickOnHeart}
+                            >
+                                <Heart />
+                            </div>
+
+                            <div
+                                className="w-6 md:w-8 md:cursor-pointer md:hover:text-orange-500"
+                                onClick={toggleNavigation}
+                            >
+                                <BurgerHeart />
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+
+        </>
     );
 };
 
