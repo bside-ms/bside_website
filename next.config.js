06146eb4f5b08a0d968ef0ab8dfc6a896a8b307b
Kennet <kennet@seebruecke.org>
2023-05-15 23:58:31 +0200

    Config: Add redirects

--- next.config.js
+++ next.config.js
@@ -1,14 +1,24 @@
+const redirects = require('./lib/redirect/redirect')
+
 /** @type {import('next').NextConfig} */
+const path = require('path')
+
 const nextConfig = {
   reactStrictMode: true,
   swcMinify: true,
 
   images: {
-    domains: ['localhost', 'cms.b-side.ovh'],
+    domains: [
+      'localhost',
+      'cms.b-side.ovh',
+      'cms.b-side.ms',
+    ],
   },
 
   // Needed to build the project as a standalone app inside the Docker image.
   output: 'standalone',
+
+  redirects,
 };
 
 module.exports = nextConfig;
