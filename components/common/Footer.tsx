23b44114a3a40d60043188c5a8cdfbc237a90315
Kennet <kennet@seebruecke.org>
2023-05-17 19:57:28 +0200

    Footer: Create links

--- components/common/Footer.tsx
+++ components/common/Footer.tsx
@@ -1,16 +1,36 @@
+import Link from 'next/link';
 import type { ReactElement } from 'react';
 
 const Footer = (): ReactElement => {
     return (
         <footer className="mb-auto">
-            <div className="bg-black mt-5">
-                <div className="py-5 text-center text-white font-bold font-serif text-lg">
-                    Impressum | Datenschutz | AGB
+            <div className="bg-black mt-5 py-2">
+                <div className="py-auto text-center text-white font-bold font-serif">
+                    <Link
+                        href="/impressum"
+                        className="text-md md:text-lg font-bold font-serif"
+                    >
+                        Impressum
+                    </Link>
+                    <span className="px-2">|</span>
+                    <Link
+                        href="/datenschutz"
+                        className="text-md md:text-lg font-bold font-serif"
+                    >
+                        Datenschutz
+                    </Link>
+                    <span className="px-2">|</span>
+                    <Link
+                        href="/kontakt"
+                        className="text-md md:text-lg font-bold font-serif"
+                    >
+                        Kontakt
+                    </Link>
                 </div>
             </div>
 
             <div className="px-2 my-4 flex justify-center text-center font-serif text-sm">
-                B-Side GmbH | B-Side Kultur e.V. | B-Side e.V.
+                Platzhalter für Footer-Text
             </div>
         </footer>
     );
