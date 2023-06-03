fbd14b568864e07d685e7e3417bc210ef85f1d1f
Kennet <kennet@seebruecke.org>
2023-06-03 10:29:23 +0200

    Create Hero-Component

---
+++ components/common/HeroImageSvg.tsx
@@ -0,0 +1,32 @@
+import Image from 'next/image';
+import type { ReactElement } from 'react';
+import ContentWrapper from 'components/common/ContentWrapper';
+
+interface Props {
+    imageSrc: string;
+    imageAlt: string;
+    title: string;
+}
+
+const HeroImageSvg = ({ imageSrc, imageAlt, title }: Props): ReactElement => {
+
+    return (
+        <ContentWrapper>
+            <div className="w-full h-32 xs:h-40 md:h-52 relative lg:mt-4">
+                <Image
+                    src={`/assets/stickFigures/${imageSrc}.svg`}
+                    alt={imageAlt}
+                    fill={true}
+                    className="object-contain"
+                    priority={true}
+                />
+            </div>
+
+            <div className="font-serif text-white bg-black mt-4 text-2xl text-center p-3">
+                {title}
+            </div>
+        </ContentWrapper>
+    );
+};
+
+export default HeroImageSvg;
