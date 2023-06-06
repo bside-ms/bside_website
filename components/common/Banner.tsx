6a94af5e7ff21014051d332b554fa90833e1d9e2
Carsten Dietzel <carsten.dietzel@check24.de>
2023-06-06 23:15:49 +0200

    No need for prop at the moment (YAGNI <3)

--- components/common/Banner.tsx
+++ components/common/Banner.tsx
@@ -1,24 +1,24 @@
+import Link from 'next/link';
 import type { ReactElement } from 'react';
 
 interface Props {
     bannerId: string;
     bannerText: string;
     bannerLink: string;
-    sticky?: boolean;
 }
 
-const Banner = ({ bannerId, bannerText, bannerLink, sticky = false }: Props): ReactElement => {
+const Banner = ({ bannerId, bannerText, bannerLink }: Props): ReactElement => {
     return (
         <div
             id={bannerId}
-            className={`${sticky ? 'sticky' : ''} top-0 left-0 right-0 z-10 h-[44px] bg-black py-2 text-center transition-opacity duration-100`}
+            className="sticky top-0 left-0 right-0 z-10 h-[44px] bg-black py-2 text-center transition-opacity duration-100"
         >
-            <a
+            <Link
                 href={bannerLink}
                 className="text-white font-serif text-sm lg:text-lg hover:bg-orange-600"
             >
                 {bannerText}
-            </a>
+            </Link>
         </div>
     );
 };
--- pages/events/[...slug].tsx
+++ pages/events/[...slug].tsx
@@ -100,7 +100,6 @@ export default ({
                     bannerId="ical-link"
                     bannerLink={`/api/ics/?eventId=${event.id}`}
                     bannerText="Veranstaltung in meinen Kalender eintragen!"
-                    sticky={true}
                 />
             )}
 
