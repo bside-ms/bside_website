e9f7d02e165f9e757831e9c3a995721b98963e08
Kennet <kennet@seebruecke.org>
2023-08-08 18:11:39 +0200

    fix: frontpage hero layout

--- components/frontPage/HouseHero.tsx
+++ components/frontPage/HouseHero.tsx
@@ -12,7 +12,7 @@ const HeroText = (): ReactElement => (
             Die B-Side in Münster
         </div>
 
-        <div className="mt-1 text-md lg:mt-3 lg:text-lg">
+        <div className="mt-1 text-md lg:mt-3 md:text-lg">
             Die B-Side ist ein offener Ort der Möglichkeiten
             am <span className="line-through">Münsteraner Hafen</span> Hawerkamp,
             der von vielen Menschen selbstorganisiert entwickelt, gestaltet und
@@ -35,35 +35,39 @@ const HouseHero = (): ReactElement => {
 
     return (
         <>
-            <div className="mt-1 md:mt-0 lg:mx-auto">
-                <div className="relative overflow-hidden lg:overflow-visible lg:flex lg:justify-center pt-5">
-                    <div className="relative lg:flex lg:justify-center">
-                        <Link href="/bside/haus" className="cursor-default lg:cursor-pointer">
-                            <div className="lg:relative h-[400px] lg:h-auto">
-                                <img
-                                    className="absolute top-[0px] left-[20px] w-[200px] lg:w-[290px] max-w-none"
-                                    src={herzPng.src}
-                                    alt="B-Side-Herz"
-                                    width={1900}
-                                    loading="eager"
-                                />
-                                <img
-                                    className="absolute lg:relative top-[55px] lg:top-0 lg:pt-[55px] left-[20px] w-[600px] lg:w-full lg:left-0 max-w-none"
-                                    src={hausPng.src}
-                                    alt="B-Side am Hafen"
-                                    width={1900}
-                                    loading="eager"
-                                />
-                            </div>
-                        </Link>
-
-                        <div className="hidden lg:block absolute right-0 bottom-0 w-[350px]">
-                            <HeroText />
+            <ContentWrapper>
+                <div className="mt-1 md:mt-0 lg:mx-auto lg:h-[560px] xl:h-[525px] 2xl:h-[580px]">
+                    <div className="relative overflow-hidden lg:overflow-visible lg:flex lg:justify-center pt-5">
+                        <div className="relative lg:flex lg:justify-start lg:mr-auto">
+                            <Link href="/bside/haus" className="cursor-default lg:cursor-pointer">
+                                <div className="lg:relative h-[400px] lg:h-auto lg:w-[700px] xl:w-[800px] 2xl:w-[900px]">
+                                    <img
+                                        className="absolute top-[0px] left-[20px] w-[200px] lg:w-[290px] max-w-none"
+                                        src={herzPng.src}
+                                        alt="B-Side-Herz"
+                                        width={1900}
+                                        loading="eager"
+                                    />
+                                    <img
+                                        className="absolute lg:relative top-[55px] lg:top-0 lg:pt-[55px] left-[20px] w-[600px] lg:w-full lg:left-0 max-w-none"
+                                        src={hausPng.src}
+                                        alt="B-Side am Hafen"
+                                        width={1900}
+                                        loading="eager"
+                                    />
+                                </div>
+                            </Link>
                         </div>
+                        <div>
 
+                            <div className="hidden lg:block absolute lg:top-[325px] xl:top-[350px] 2xl:top-[375px] right-0 bottom-0 lg:w-[400px] xl:w-[550px] 2xl:w-[500px]">
+                                <HeroText />
+                            </div>
+
+                        </div>
                     </div>
                 </div>
-            </div>
+            </ContentWrapper>
 
             <div className="lg:hidden px-0 mb-2">
                 <ContentWrapper>
