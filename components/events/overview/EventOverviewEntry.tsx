93048bba91cbad976b89b44e5be2055da34c0458
Kennet <kennet@seebruecke.org>
2023-08-24 09:10:21 +0200

    fix: create labels for event links

--- components/events/overview/EventOverviewEntry.tsx
+++ components/events/overview/EventOverviewEntry.tsx
@@ -26,6 +26,7 @@ const EventOverviewEntry = ({ event, index }: Props): ReactElement => {
                 key={`event-${event.id}-link`}
                 href={`/events/${createEventSlug(event)}`}
                 className="px-3 md:px-4 pt-1 md:pt-2 flex gap-3 hover:text-orange-500"
+                aria-label={`Erfahre mehr über die Veranstaltung "${event.title}".`}
             >
                 <div className="w-14">{formatDate(new Date(event.eventStart), 'HH:mm')}</div>
                 <div key={`event-${event.id}-title`} className="truncate flex-1 font-bold">{event.title}</div>
@@ -33,7 +34,11 @@ const EventOverviewEntry = ({ event, index }: Props): ReactElement => {
             </Link>
 
             <div className="px-3 md:px-4 pb-1 md:pb-2 flex gap-3 relative">
-                <Link href={`/events/${createEventSlug(event)}`} className="absolute top-0 bottom-0 right-0 left-0 hover:text-orange-500" />
+                <Link
+                    href={`/events/${createEventSlug(event)}`}
+                    className="absolute top-0 bottom-0 right-0 left-0 hover:text-orange-500"
+                    aria-label={`Erfahre mehr über die Veranstaltung "${event.title}".`}
+                />
                 <div className="w-0 sm:w-14" />
                 {event.category?.map(cat => (
                     <div key={`event-title-${event.id}-${cat}`} className="truncate px-1 my-auto leading-6 text-sm italic">
