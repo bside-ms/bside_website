1a15460700c55a048fc896a26eb6f0fe5748788d
Kennet <kennet@seebruecke.org>
2023-08-24 08:35:43 +0200

    Fix: adjust favicons

--- components/layout/next/NextHead.tsx
+++ components/layout/next/NextHead.tsx
@@ -34,8 +34,8 @@ const NextHead = ({ title, description, url }: Props): ReactElement => {
             <link rel="apple-touch-icon" href="/apple-touch-icon.png" />
             <link rel="shortcut icon" href="/favicon.ico" />
             <link rel="icon" type="image/png" href="/favicon-32x32.png" sizes="32x32" />
-            <link rel="icon" type="image/png" href="/android-chrome-512x512.png" sizes="512x512" />
-            <link rel="icon" type="image/png" href="/android-chrome-192x192.png" sizes="192x192" />
+            <link rel="icon" type="image/png" href="/favicon-512.png" sizes="512x512" />
+            <link rel="icon" type="image/png" href="/favicon-192.png" sizes="192x192" />
 
             <link
                 rel="canonical"
