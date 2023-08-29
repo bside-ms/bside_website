7443c0c8ffeccd0fb97a84d9af0ddf8ca448287d
Carsten Dietzel <carsten.dietzel@check24.de>
2023-08-29 19:43:01 +0200

    Link to parallax again

--- components/frontPage/FrontPageImage.tsx
+++ components/frontPage/FrontPageImage.tsx
@@ -9,7 +9,7 @@ const FrontPageImage = (): ReactElement => (
     <div className="mt-1 md:mt-0 lg:mx-auto lg:h-[560px] xl:h-[525px] 2xl:h-[580px]">
         <div className="relative overflow-hidden lg:overflow-visible lg:flex lg:justify-center pt-5">
             <div className="relative lg:flex lg:justify-start lg:mr-auto">
-                <Link href="/bside" className="cursor-default lg:cursor-pointer">
+                <Link href="/bside/haus" className="cursor-default lg:cursor-pointer">
                     <div className="lg:relative h-[400px] lg:h-auto lg:w-[700px] xl:w-[800px] 2xl:w-[900px]">
                         <Image
                             className="absolute top-[0px] left-[20px] w-[200px] lg:w-[290px] max-w-none"
