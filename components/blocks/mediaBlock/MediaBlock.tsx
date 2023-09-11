8bbea4997f2b88e5329b56c4e332fc976a857c4c
Kennet <kennet@seebruecke.org>
2023-09-11 04:03:07 +0200

    fix: reduce function complexity

--- components/blocks/mediaBlock/MediaBlock.tsx
+++ components/blocks/mediaBlock/MediaBlock.tsx
@@ -5,37 +5,35 @@ import ContentWrapper from '@/components/layout/ContentWrapper';
 import isEmptyString from '@/lib/common/helper/isEmptyString';
 import type { MediaBlockProps } from '@/types/payload/Blocks';
 
+const WideMediaBlock = (url: string, effects: Array<'blur' | 'grayscale' | 'desaturated' | 'darker'>): ReactElement | null => {
+    return (
+        <div className="w-full px-4 lg:w-[60rem] xl:w-[80rem] lg:mx-auto">
+            <div
+                className={clsx(
+                    'bg-fill bg-center w-full h-52 md:h-72 my-8',
+                    effects.includes('blur') && 'blur-[2px]',
+                    effects.includes('grayscale') && 'grayscale',
+                    effects.includes('desaturated') && 'saturate-50',
+                    effects.includes('darker') && 'brightness-50',
+                )}
+                style={{ backgroundImage: `url(${url})` }}
+            />
+        </div>
+    );
+};
+
 const MediaBlock = ({ media, caption = '', size, effects }: MediaBlockProps): ReactElement | null => {
 
     if (typeof media === 'string') {
-        // eslint-disable-next-line no-console
-        console.warn('Media of type string currently not supported');
-
         return null;
     }
 
     if (isEmptyString(media.url)) {
-        // eslint-disable-next-line no-console
-        console.warn('URL of media must not be empty');
-
         return null;
     }
 
     if (size === 'wide' && media.sizes?.wide?.url !== undefined) {
-        return (
-            <div className="w-full px-4 lg:w-[60rem] xl:w-[80rem] lg:mx-auto">
-                <div
-                    className={clsx(
-                        'bg-fill bg-center w-full h-52 md:h-72 my-8',
-                        ((effects?.includes('blur')) ?? false) && 'blur-[2px]',
-                        ((effects?.includes('grayscale')) ?? false) && 'grayscale',
-                        ((effects?.includes('desaturated')) ?? false) && 'saturate-50',
-                        ((effects?.includes('darker')) ?? false) && 'brightness-50',
-                    )}
-                    style={{ backgroundImage: `url(${media.sizes.wide.url})` }}
-                />
-            </div>
-        );
+        return WideMediaBlock(media.sizes.wide.url, effects ?? []);
     }
 
     // ToDo: Implement the square version of the image.
