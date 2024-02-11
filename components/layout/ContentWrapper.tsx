f8c535a921397227051c81e5d9302a4537601aa9
Carsten Dietzel <carsten.dietzel@check24.de>
2024-02-11 19:10:37 +0100

    No padding top for small devices

--- components/layout/ContentWrapper.tsx
+++ components/layout/ContentWrapper.tsx
@@ -9,7 +9,7 @@ const ContentWrapper = ({ className = '', children }: Props): ReactElement => {
 
     return (
         <div className="flex-grow px-4 md:px-8">
-            <div className={clsx('w-full lg:w-[54rem] xl:w-[70rem] mx-auto py-4', className)}>
+            <div className={clsx('w-full lg:w-[54rem] xl:w-[70rem] mx-auto pb-4 lg:pt-4', className)}>
                 {children}
             </div>
         </div>
