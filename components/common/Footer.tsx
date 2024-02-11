940dc70ff85c075eab196228553f26c9e6a95be6
Carsten Dietzel <carsten.dietzel@check24.de>
2024-02-11 15:20:42 +0100

    Remove obsolete ContentWrapper

--- components/common/Footer.tsx
+++ components/common/Footer.tsx
@@ -1,4 +1,3 @@
-import { Fragment } from 'react';
 import Link from 'next/link';
 import { useRouter } from 'next/router';
 import type { ReactElement, ReactNode } from 'react';
@@ -60,62 +59,59 @@ const Footer = ({ children }: FooterProps): ReactElement => {
     const { locale } = useRouter();
 
     return (
-        <Fragment>
-            <ContentWrapper />
-            <footer className="mb-auto" key="footer">
-                <div className="bg-black mt-5 py-2">
-                    <div className="py-auto text-center text-white font-bold font-serif">
-                        <Link
-                            href="/impressum"
-                            aria-label={locale !== 'en' ? 'Alle rechtlichen Angaben sind hier zu finden' : 'All legal information can be found here'}
-                            className="text-md md:text-lg font-bold font-serif hover:text-orange-500"
-                        >
-                            {locale !== 'en' ? 'Impressum' : 'Imprint'}
-                        </Link>
-                        <span className="px-2">|</span>
-                        <Link
-                            href="/datenschutz"
-                            aria-label={locale !== 'en' ? 'Erfahre mehr darüber, wie wir mit deinen Daten umgehen' : 'Learn more about how we handle your data'}
-                            className="text-md md:text-lg font-bold font-serif hover:text-orange-500"
-                        >
-                            {locale !== 'en' ? 'Datenschutz' : 'Privacy Policy'}
-                        </Link>
-                        <span className="px-2">|</span>
-                        <Link
-                            href="/kontakt"
-                            aria-label={locale !== 'en' ? 'Trete mit uns in den Kontakt' : 'Get in touch with us'}
-                            className="text-md md:text-lg font-bold font-serif hover:text-orange-500"
-                        >
-                            {locale !== 'en' ? 'Kontakt' : 'Contact Us'}
-                        </Link>
+        <footer className="mb-auto" key="footer">
+            <div className="bg-black mt-5 py-2">
+                <div className="py-auto text-center text-white font-bold font-serif">
+                    <Link
+                        href="/impressum"
+                        aria-label={locale !== 'en' ? 'Alle rechtlichen Angaben sind hier zu finden' : 'All legal information can be found here'}
+                        className="text-md md:text-lg font-bold font-serif hover:text-orange-500"
+                    >
+                        {locale !== 'en' ? 'Impressum' : 'Imprint'}
+                    </Link>
+                    <span className="px-2">|</span>
+                    <Link
+                        href="/datenschutz"
+                        aria-label={locale !== 'en' ? 'Erfahre mehr darüber, wie wir mit deinen Daten umgehen' : 'Learn more about how we handle your data'}
+                        className="text-md md:text-lg font-bold font-serif hover:text-orange-500"
+                    >
+                        {locale !== 'en' ? 'Datenschutz' : 'Privacy Policy'}
+                    </Link>
+                    <span className="px-2">|</span>
+                    <Link
+                        href="/kontakt"
+                        aria-label={locale !== 'en' ? 'Trete mit uns in den Kontakt' : 'Get in touch with us'}
+                        className="text-md md:text-lg font-bold font-serif hover:text-orange-500"
+                    >
+                        {locale !== 'en' ? 'Kontakt' : 'Contact Us'}
+                    </Link>
+                </div>
+            </div>
+
+            {/* Only used for inViewRefs. */}
+            {children}
+
+            <ContentWrapper className="!pb-2">
+                <div className="grid md:grid-cols-3 text-center text-sm">
+                    <div className="mb-2 md:my-0">
+                        <p className="font-serif font-bold mb-2">B-Side GmbH</p>
+                        <FooterLink linkUrl="/quartier" linkText={locale !== 'en' ? 'Über uns' : 'About us'} />
+                        <FooterLink linkUrl="/impressum" linkText={locale !== 'en' ? 'Impressum' : 'Imprint'} />
+                    </div>
+                    <div className="mb-2 md:my-0">
+                        <p className="font-serif font-bold mb-2">{locale !== 'en' ? 'Zentrale Teile des Zentrums' : 'Other Central Parts'}</p>
+                        <FooterLink linkUrl="/kultur" linkText="B-Side Kultur e.V." />
+                        <FooterLink linkUrl="/bside/kollektiv" linkText={locale !== 'en' ? 'B-Side Kollektiv' : 'B-Side Collective'} />
+                    </div>
+                    <div className="">
+                        <p className="font-serif font-bold mb-2">{locale !== 'en' ? 'Andere Platformen' : 'Other Platforms'}</p>
+                        <Instagram />
+                        <GithubLink />
                     </div>
                 </div>
+            </ContentWrapper>
 
-                {/* Only used for inViewRefs. */}
-                {children}
-
-                <ContentWrapper className="!pb-2">
-                    <div className="grid md:grid-cols-3 text-center text-sm">
-                        <div className="mb-2 md:my-0">
-                            <p className="font-serif font-bold mb-2">B-Side GmbH</p>
-                            <FooterLink linkUrl="/quartier" linkText={locale !== 'en' ? 'Über uns' : 'About us'} />
-                            <FooterLink linkUrl="/impressum" linkText={locale !== 'en' ? 'Impressum' : 'Imprint'} />
-                        </div>
-                        <div className="mb-2 md:my-0">
-                            <p className="font-serif font-bold mb-2">{locale !== 'en' ? 'Zentrale Teile des Zentrums' : 'Other Central Parts'}</p>
-                            <FooterLink linkUrl="/kultur" linkText="B-Side Kultur e.V." />
-                            <FooterLink linkUrl="/bside/kollektiv" linkText={locale !== 'en' ? 'B-Side Kollektiv' : 'B-Side Collective'} />
-                        </div>
-                        <div className="">
-                            <p className="font-serif font-bold mb-2">{locale !== 'en' ? 'Andere Platformen' : 'Other Platforms'}</p>
-                            <Instagram />
-                            <GithubLink />
-                        </div>
-                    </div>
-                </ContentWrapper>
-
-            </footer>
-        </Fragment>
+        </footer>
     );
 };
 
