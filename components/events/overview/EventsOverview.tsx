e91f7e7d4459d080893cc5b990bfa04df99a875b
Carsten Dietzel <carsten.dietzel@check24.de>
2024-03-09 08:44:06 +0100

    Persist current page in URL

--- components/events/overview/EventsOverview.tsx
+++ components/events/overview/EventsOverview.tsx
@@ -1,7 +1,6 @@
-import { useCallback, useMemo, useRef, useState } from 'react';
+import { useCallback, useEffect, useMemo, useRef, useState } from 'react';
 import { useRouter } from 'next/router';
 import type { ReactElement } from 'react';
-import EventOverviewEmpty from '@/components/events/overview/EventOverviewEmpty';
 import EventOverviewEntry from '@/components/events/overview/EventOverviewEntry';
 import EventsPagination from '@/components/events/overview/EventsPagination';
 import delayExecution from '@/lib/common/helper/delayExecution';
@@ -44,13 +43,44 @@ const EventCategoryFilter = ({
     );
 };
 
+const pageParam = 'page';
+
 const EventsOverview = ({ title = '', eventsOnPage }: Props): ReactElement => {
 
-    const [page, setPage] = useState(1);
+    const searchParams = new URLSearchParams(typeof window === 'undefined' ? undefined : window.location.search);
+
+    const [page, setPage] = useState(Number(searchParams.get(pageParam) ?? 1));
+
+    useEffect(() => {
+        const url = new URL(window.location.href);
+
+        if (page === 1) {
+            url.searchParams.delete(pageParam);
+        } else {
+            url.searchParams.set(pageParam, page.toString());
+        }
+
+        window.history.replaceState(window.history.state, '', url.href);
+    }, [page]);
 
     const [categories, setCategories] = useState<Array<EventCategory>>([]);
 
     const paginatedEvents = useAllEvents(eventsOnPage, page, categories);
+
+    useEffect(() => {
+        if (paginatedEvents === undefined) {
+            return;
+        }
+
+        if (paginatedEvents.totalPages < page) {
+            setPage(paginatedEvents.totalPages);
+        } else if (paginatedEvents.page !== undefined && paginatedEvents.page !== page) {
+            setPage(paginatedEvents.page);
+        }
+
+        // Does not have page dependency on purpose.
+        // eslint-disable-next-line react-hooks/exhaustive-deps
+    }, [paginatedEvents]);
 
     const allAvailableCategories = useAvailableCategoryFilters(eventsOnPage);
 
@@ -82,10 +112,6 @@ const EventsOverview = ({ title = '', eventsOnPage }: Props): ReactElement => {
 
         eventsScrollAnchorRef.current?.scrollIntoView({ behavior: 'smooth' });
     }, []);
-
-    if (paginatedEvents?.docs.length === 0) {
-        return EventOverviewEmpty({ title });
-    }
 
     return (
         <div className="relative">
@@ -121,7 +147,7 @@ const EventsOverview = ({ title = '', eventsOnPage }: Props): ReactElement => {
                 )}
 
                 <div>
-                    {groupByDay.map(([date, events]) => (
+                    {groupByDay.length > 0 ? groupByDay.map(([date, events]) => (
                         <div key={date.toString()} className="mb-2">
                             <div className="px-3 md:px-4 py-1 md:py-2 bg-black text-white font-serif font-bold">
                                 {formatDate(date, 'EE dd. MMMM yy', locale)}
@@ -131,11 +157,23 @@ const EventsOverview = ({ title = '', eventsOnPage }: Props): ReactElement => {
                                 <EventOverviewEntry key={`event-${event.id}`} event={event} index={index} />
                             ))}
                         </div>
-                    ))}
+                    )) : (
+                        <div className="mb-2">
+                            <div className="px-3 md:px-4 py-1 md:py-2 bg-black text-white font-serif font-bold">
+                                {locale === 'de' ? 'Nichts gefunden!' : 'Nothing found!'}
+                            </div>
+
+                            <div className="py-1 md:py-2 flex gap-3">
+                                <div className="w-full">
+                                    {locale === 'de' ? 'Hier gibt es aktuell keine Termine.' : 'There are currently no events.'}
+                                </div>
+                            </div>
+                        </div>
+                    )}
                 </div>
 
                 {eventsOnPage?.pagination === true && paginatedEvents !== undefined && paginatedEvents.totalPages > 1 && (
-                    <EventsPagination paginatedEvents={paginatedEvents} page={page} setPage={handleSetPage} />
+                    <EventsPagination paginatedEvents={paginatedEvents} currentPage={page} setPage={handleSetPage} />
                 )}
             </div>
         </div>
